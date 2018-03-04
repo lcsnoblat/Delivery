a324375eab7cbc321cbe1dcb109c618e3df4e272
Lucas Noblat <lucasnoblat123@gmail.com>
2018-03-04 11:57:09 -0300

    Adicionar arquivos de projeto.

---
+++ LumaDelivery/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LumaDelivery.Startup))]
+namespace LumaDelivery
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
